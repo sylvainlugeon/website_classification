bb31c8bd486295469543811fec479de2e5f42924
Lugeon Sylvain Maurice Tuari <lugeon@iccluster111.iccluster.epfl.ch>
2020-10-01 09:37:30 +0200

    exception handling when embedding json

--- .ipynb_checkpoints/embed_json-checkpoint.py
+++ .ipynb_checkpoints/embed_json-checkpoint.py
@@ -8,6 +8,8 @@ from bs4 import BeautifulSoup
 model = api.load('word2vec-google-news-300')
 
 def word2vec_avg(str):
+    if str == '':
+        return None
     acc = np.zeros(300)
     counter = 0
     for word in str.split(' '):
@@ -24,15 +26,18 @@ def word2vec_avg(str):
 
 def clean_df(df):
     df_valid = df[df.html.notnull()]
-    df_valid = df_valid[df_valid.apply(lambda x: x.html != '', axis=1)]
     return df_valid
 
 def clean_html(body):
-    soup = BeautifulSoup(body, 'html.parser')
-    a = soup.get_text(' ').replace('\n', '').lower()
-    b = re.sub(r"[^a-z ]+","", a)
-    c = ' '.join(b.split())
-    return c
+    try:
+        soup = BeautifulSoup(body, 'html.parser')
+        a = soup.get_text(' ').replace('\n', '').lower()
+        b = re.sub(r"[^a-z ]+","", a)
+        c = ' '.join(b.split())
+        return c
+    except:
+        return ''
+    
 
 def create_w2v_avg(df):
     df_valid = clean_df(df)
@@ -40,15 +45,18 @@ def create_w2v_avg(df):
     return df_valid[['emb', 'cat']]
 
 
-
-reader = pd.read_json("../data/websites_1000_5cat_html.json.gz", orient='records', lines=True, chunksize=100)
+chunksize = 1000
+reader = pd.read_json("/dlabdata1/lugeon/websites_40000_5cat_html.json.gz", orient='records', lines=True, chunksize=chunksize)
 
 df_main = pd.DataFrame([])
 
+i = 0
 for chunk in reader:
     df_chunk = pd.DataFrame(chunk)
     df_chunk = create_w2v_avg(df_chunk)
     df_main = pd.concat((df_main, df_chunk))
+    i +=1 
+    print('{} embeddings done'.format(chunksize*i))
     
-df_main.to_csv('test.gz', compression='gzip')
+df_main.to_csv('/dlabdata1/lugeon/test.gz', compression='gzip')
     
