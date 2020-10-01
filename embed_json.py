f4893dc92a19eaab3a7f7252ccfbf884567157ad
Sylvain Lugeon <sylvain.lugeon@epfl.ch>
2020-10-01 12:28:49 +0200

    embed json with mp

--- embed_json.py
+++ embed_json.py
@@ -3,6 +3,7 @@ import numpy as np
 import gensim.downloader as api
 import re
 from bs4 import BeautifulSoup
+import multiprocessing as mp
 
 
 model = api.load('word2vec-google-news-300')
@@ -38,25 +39,33 @@ def clean_html(body):
     except:
         return ''
     
+def clean_and_embed(str):
+    return word2vec_avg(clean_html(str))
+    
 
-def create_w2v_avg(df):
-    df_valid = clean_df(df)
-    df_valid['emb'] = df_valid.apply(lambda x: word2vec_avg(clean_html(x.html)), axis=1)
-    return df_valid[['emb', 'cat']]
+def create_w2v_embeddings(df, pool):
+    df['emb'] = pool.map(clean_and_embed, df.html)
+    return df[['emb', 'cat']]
 
 
 chunksize = 1000
-reader = pd.read_json("/dlabdata1/lugeon/websites_40000_5cat_html.json.gz", orient='records', lines=True, chunksize=chunksize)
+path = "/Users/sylvainlugeon/Documents/EPFL/semester_project/data/websites_1000_5cat_html.json.gz"
+reader = pd.read_json(path, orient='records', lines=True, chunksize=chunksize)
 
 df_main = pd.DataFrame([])
+pool = mp.Pool(mp.cpu_count())
 
 i = 0
 for chunk in reader:
     df_chunk = pd.DataFrame(chunk)
-    df_chunk = create_w2v_avg(df_chunk)
+    df_chunk = clean_df(df_chunk)
+    df_chunk = create_w2v_embeddings(df_chunk, pool)
     df_main = pd.concat((df_main, df_chunk))
     i +=1 
     print('{} embeddings done'.format(chunksize*i))
+
+pool.close()
+pool.join()
     
-df_main.to_csv('/dlabdata1/lugeon/test.gz', compression='gzip')
+df_main.to_csv('test.gz', compression='gzip')
     
